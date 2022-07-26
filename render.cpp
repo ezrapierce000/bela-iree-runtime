0c482dc9090fabcd514a3533b7831f2c285dd788
ezrapierce000 <ezrapierce@cmail.carleton.ca>
2022-07-25 22:02:59 -0400

    runtime notes.

--- render.cpp
+++ render.cpp
@@ -2,15 +2,26 @@
 #include <iree/runtime/api.h>
 
 bool iree_runtime_setup(BelaContext* context, void* userData){
-	return true;
+	// get filepath of module(s)
+	// setup runtime instance
+	// initialize module functions
+	// allocate input and output buffer
+	// setup session
+
+
+
 }
+
 
 
 void iree_runtime_render(BelaContext* context, void* userData){
 	volatile int a = 6;
 	a += 7;
+	// invoke
 }
 
 void iree_runtime_cleanup(BelaContext *context, void *userData){
-	int b = 9;
+
+	// release runtime instance
 }
+
