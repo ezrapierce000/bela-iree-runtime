ceb58faa820389a1439f3cb4f6f0897b037dcc04
ezrapierce000 <ezrapierce@cmail.carleton.ca>
2022-08-05 10:03:09 -0400

    added aux task.

--- render.c
+++ render.c
@@ -6,20 +6,32 @@
 #include "iree/vm/api.h"
 #include "iree/vm/bytecode_module.h"
 
+#define M_PI 3.14159
+
 // TODO: read this in via command line
 char* module_file_path = "/root/module.vmfb";
 iree_runtime_instance_t* instance = NULL;
 iree_runtime_session_t* session = NULL;
 iree_hal_device_t* local_sync_device = NULL;
-iree_hal_buffer_view_t* input_hal_buffer_view;
-iree_hal_buffer_view_t* output_hal_buffer_view;
-iree_hal_buffer_t* input_buffer;
 iree_runtime_call_t module_call;
-iree_hal_dim_t shape[2] = {1024, 1};
+iree_hal_buffer_view_t* input_hal_buffer_view[2];
+iree_hal_buffer_view_t* output_hal_buffer_view = NULL;
+
+// TODO: set this based on the input size of the model automatically?
+const uint32_t block_size = 1024;
+const iree_hal_dim_t shape[2] = {1024, 1};
+
+float gFrequency = 440.0;
+float gPhase;
+float gInverseSampleRate;
+
+AuxiliaryTask gIREETask;
+void bela_iree_invoke();
 
 
 
 bool iree_runtime_setup(BelaContext* context, void* userData){
+	iree_status_t status;
 	// get filepath of module(s)
 	// setup runtime instance, an instance can run multiple sessions
 	iree_runtime_instance_options_t instance_options; // holds API version and driver registry
@@ -28,7 +40,7 @@ bool iree_runtime_setup(BelaContext* context, void* userData){
 	
 	iree_runtime_instance_options_use_all_available_drivers(&instance_options);
 
-	iree_status_t status = iree_runtime_instance_create(
+	status = iree_runtime_instance_create(
 		&instance_options, iree_allocator_system(), &instance
 	);
 
@@ -42,20 +54,32 @@ bool iree_runtime_setup(BelaContext* context, void* userData){
 		instance, iree_make_cstring_view("local-sync"), &local_sync_device
 	));
 
+	if(!iree_status_is_ok(status)){
+		iree_status_fprint(stderr, status);
+		return false; // failed to create device
+	}
+
 	//setup session
 	iree_runtime_session_options_t session_options;
   	iree_runtime_session_options_initialize(&session_options);
 	status = iree_runtime_session_create_with_device(
 		instance, &session_options, local_sync_device,
 		iree_runtime_instance_host_allocator(instance), &session);
-	//iree_hal_device_release(local_sync_device);
+	iree_hal_device_release(local_sync_device);
 
-	if (iree_status_is_ok(status)) {
-    	status = iree_runtime_session_append_bytecode_module_from_file(session,
+	status = iree_runtime_session_append_bytecode_module_from_file(session,
                                                                module_file_path);;
-  	}
-	IREE_RETURN_IF_ERROR(iree_runtime_call_initialize_by_name(
-		session, iree_make_cstring_view("module.main"), &module_call));
+	if(!iree_status_is_ok(status)){
+		iree_status_fprint(stderr, status);
+		return false; // failed to load bytecode module
+	}
+	iree_runtime_call_initialize_by_name(
+		session, iree_make_cstring_view("module.main"), &module_call);
+
+	if(!iree_status_is_ok(status)){
+		iree_status_fprint(stderr, status);
+		return false; // failed to initialize module function
+	}
 
 	iree_hal_allocator_t* device_allocator =
 		iree_runtime_session_device_allocator(session);
@@ -63,90 +87,115 @@ bool iree_runtime_setup(BelaContext* context, void* userData){
 		iree_runtime_session_host_allocator(session);
   	status = iree_ok_status();
 
-
-
+	IREE_RETURN_IF_ERROR(iree_hal_buffer_view_allocate_buffer(
+		iree_hal_device_allocator(local_sync_device), IREE_ARRAYSIZE(shape), shape,
+		IREE_HAL_ELEMENT_TYPE_FLOAT_32, IREE_HAL_ENCODING_TYPE_DENSE_ROW_MAJOR,
+		(iree_hal_buffer_params_t){
+			.type = IREE_HAL_MEMORY_TYPE_DEVICE_LOCAL,
+			.usage = IREE_HAL_BUFFER_USAGE_DEFAULT,
+		},
+		/* initial data */ iree_make_const_byte_span(NULL, 1024), &input_hal_buffer_view[0]));
 
 	IREE_RETURN_IF_ERROR(iree_hal_buffer_view_allocate_buffer(
 		iree_hal_device_allocator(local_sync_device), IREE_ARRAYSIZE(shape), shape,
 		IREE_HAL_ELEMENT_TYPE_FLOAT_32, IREE_HAL_ENCODING_TYPE_DENSE_ROW_MAJOR,
 		(iree_hal_buffer_params_t){
-			.type = IREE_HAL_MEMORY_TYPE_HOST_LOCAL,
+			.type = IREE_HAL_MEMORY_TYPE_DEVICE_LOCAL,
 			.usage = IREE_HAL_BUFFER_USAGE_DEFAULT,
 		},
-		/* initial data */ iree_make_const_byte_span(4, 1024), &input_hal_buffer_view));
+		/* initial data */ iree_make_const_byte_span(NULL, 1024), &input_hal_buffer_view[1]));
+	// ensure output buffer is zeroed out to avoid clicks on startup
 
-	IREE_RETURN_IF_ERROR(iree_hal_buffer_view_allocate_buffer(
-		iree_hal_device_allocator(local_sync_device), IREE_ARRAYSIZE(shape), shape,
-		IREE_HAL_ELEMENT_TYPE_FLOAT_32, IREE_HAL_ENCODING_TYPE_DENSE_ROW_MAJOR,
-		(iree_hal_buffer_params_t){
-			.type = IREE_HAL_MEMORY_TYPE_HOST_LOCAL,
-			.usage = IREE_HAL_BUFFER_USAGE_DEFAULT,
-		},
-		/* initial data */ iree_make_const_byte_span(NULL, 1024), &output_hal_buffer_view));
+	status = iree_runtime_call_inputs_push_back_buffer_view(
+		&module_call, input_hal_buffer_view[0]);
+		
 
-
-	iree_runtime_call_inputs_push_back_buffer_view(
-		&module_call, input_hal_buffer_view);
-
-	iree_vm_list_retain(module_call.inputs);
-	iree_vm_list_retain(module_call.outputs);
+	if((gIREETask = Bela_createAuxiliaryTask(&bela_iree_invoke, 90, "iree", NULL)) == 0)
+		return false;
 	
-	// delete dynamically allocated lists in call then statically alocate them
-	/*
-	iree_vm_list_release(module_call.inputs);
-	iree_vm_list_release(module_call.outputs);
-
-	iree_vm_type_def_t buffer_view_type_def =
-      iree_vm_type_def_make_ref_type(iree_hal_buffer_type_id());
-
-  	iree_byte_span_t input_list_storage = iree_make_byte_span(
-      	NULL, iree_vm_list_storage_size(&buffer_view_type_def, 1));
-  	IREE_RETURN_IF_ERROR(
-      	iree_vm_list_initialize(input_list_storage, &buffer_view_type_def,
-                              	1, &module_call.inputs));*/
-								
-/*
-	// adding allocated buffer to call inputs
-	// iree_vm_list_t* inputs = iree_runtime_call_inputs(&module_call);
-  	iree_vm_ref_t in_ref = {0};
-	IREE_RETURN_IF_ERROR(iree_vm_ref_wrap_assign(
-      	input_hal_buffer_view, iree_hal_buffer_view_type_id(), &in_ref));
- 	iree_vm_list_push_ref_retain(module_call.inputs, &in_ref);
+	// run the model
+	status = iree_runtime_call_invoke(&module_call, /*flags*/0);
+	if(!iree_status_is_ok(status)){
+		iree_status_fprint(stderr, status);
+		return false;
+	}
 	
-	iree_vm_ref_t out_ref = {0};
-	IREE_RETURN_IF_ERROR(iree_vm_ref_wrap_assign(
-      	output_hal_buffer_view, iree_hal_buffer_view_type_id(), &out_ref));
- 	iree_vm_list_push_ref_retain(module_call.outputs, &out_ref);
-*/
+  if (iree_status_is_ok(status)) {
+    // This prints the buffer view out but an application could read its
+    // contents, pass it to another call, etc.
+    //status = iree_hal_buffer_view_fprint(
+      //  stdout, ret0, /*max_element_count=*/4096, host_allocator);
+  }
+
+	fprintf(stdout, "%d\n", iree_vm_list_size(module_call.outputs));
+	iree_runtime_call_outputs_pop_front_buffer_view(&module_call, &output_hal_buffer_view);
+	// read the output
+	// how to get output buffer??
 
 
-	
-	//iree_runtime_session_retain(session);
     fprintf(stdout, "\n * \n");
 
+	gInverseSampleRate = 1.0 / context->audioSampleRate;
+	gPhase = 0.0;
+
 	return true;
+}
+
+void test_sine(BelaContext* context){
+	for(unsigned int n = 0; n < context->audioFrames; n++) {
+		float out = 0.8f * sinf(gPhase);
+		gPhase += 2.0f * (float)M_PI * gFrequency * gInverseSampleRate;
+		if(gPhase > M_PI)
+			gPhase -= 2.0f * (float)M_PI;
+
+		for(unsigned int channel = 0; channel < context->audioOutChannels; channel++) {
+			audioWrite(context, n, channel, out);
+		}
+	}
 }
 
 
 
 void iree_runtime_render(BelaContext* context, void* userData){
-	//iree_vm_list_retain(module_call.inputs);
-	// iree_vm_list_t* inputs = iree_runtime_call_inputs(&module_call);
+	static bool inputBufferToggle = false; // denotes which buffer is being processed and which is being filled
+	static uint32_t bufferByteWriteCount = 0;
+	iree_status_t status;
+	test_sine(context);
+
+	// once last call is done, toggle, swap buffers and invoke again
+	if((iree_vm_list_size(module_call.outputs) > 0) && (bufferByteWriteCount >= block_size)){ // if output is ready and previous buffer is all read out
+		output_hal_buffer_view = NULL;
+		status = iree_runtime_call_outputs_pop_front_buffer_view(&module_call, &output_hal_buffer_view);
+		iree_runtime_call_reset(&module_call);
+		iree_runtime_call_inputs_push_back_buffer_view(&module_call, input_hal_buffer_view[inputBufferToggle]);
+		inputBufferToggle = ~inputBufferToggle;
+		bufferByteWriteCount = 0;
+		Bela_scheduleAuxiliaryTask(gIREETask);
+		//rt_printf("toggle.\n");
+	}
+	else if(bufferByteWriteCount >= block_size){
+		return false; // model missed latency deadline
+	}
+
+	// write data into preparation hal buffer for IREE
+	uint32_t dataTransferSize = min(block_size - bufferByteWriteCount, context->audioFrames);
+	status = iree_hal_buffer_map_write(iree_hal_buffer_view_buffer(input_hal_buffer_view[inputBufferToggle]),
+		bufferByteWriteCount, context->audioIn, dataTransferSize);
+	// write data out from previous invocation
+	status = iree_hal_buffer_map_read(iree_hal_buffer_view_buffer(output_hal_buffer_view),
+		bufferByteWriteCount, context->audioOut, dataTransferSize);
+	bufferByteWriteCount += dataTransferSize;
 
 
-	// list and hal buffer view are NULL?
-	//
+}
 
-	iree_hal_buffer_view_t* input_buffer_view = (iree_hal_buffer_view_t*)iree_vm_list_get_ref_deref(module_call.inputs, 0, iree_hal_buffer_view_get_descriptor());
-	//iree_hal_buffer_map_write(
-	//	iree_hal_buffer_view_buffer(input_buffer_view),
-	//	0, context->audioIn, context->audioFrames*4);
-	// iree_vm_list_release(module_call.inputs);
-
+void bela_iree_invoke(){
+	iree_runtime_call_invoke(&module_call, /*flags*/ 0);
 }
 
 void iree_runtime_cleanup(BelaContext *context, void *userData){
 	iree_runtime_instance_release(instance);
-	// release runtime instance
+	iree_runtime_session_release(session);
+	// TODO: release local-sync device?
 }
 
